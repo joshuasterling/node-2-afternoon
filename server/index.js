b6e26cd40611c19dc328da08e4eaec2ab5fd7ea4
Joshua Smith <joshuasmith@Joshs-MacBook-Pro.local>
2020-02-05 15:25:46 -0700

    actually completed all assignments

--- server/index.js
+++ server/index.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const app = express();
-app.use(express.json());
-const PORT = 3001;
 const messages = require('./controllers/messages_controller');
 
-
+app.use(express.json());
+app.use(express.static(__dirname + '/../public/build'));
 
 app.post('/api/messages', messages.create);
 app.get('/api/messages', messages.read);
 app.put('/api/messages/:id', messages.update);
 app.delete('/api/messages/:id', messages.delete);
 
-
-
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })
